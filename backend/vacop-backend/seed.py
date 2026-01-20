66b1ec8aa861e9646cafcece277a0b95d7b6b701
OlivierCr <ocrampette@gmail.com>
2026-01-20 19:53:28 +0100

    mise en place db et update docker

--- backend/vacop-backend/seed.py
+++ backend/vacop-backend/seed.py
@@ -1,20 +1,29 @@
 from backend.app import create_app
 from backend.extensions import db, bcrypt
 from backend.models import User
+import time
+from sqlalchemy.exc import OperationalError
 
 app = create_app()
 
 def seed_database():
     with app.app_context():
-        print("Création des tables...")
-        db.create_all()
-        if not User.query.filter_by(username='admin').first():
-            print("Création de l'admin...")
-            hashed_pw = bcrypt.generate_password_hash('vacop_admin_2026').decode('utf-8')
-            admin = User(username='admin', password_hash=hashed_pw, role='admin')
-            db.session.add(admin)
-            db.session.commit()
-            print("Admin créé.")
+        # Boucle de tentative de connexion (max 60s)
+        for _ in range(30):
+            try:
+                print("Tentative de connexion à la DB...")
+                db.create_all()
+                if not User.query.filter_by(username='admin').first():
+                    print("Création de l'admin...")
+                    hashed_pw = bcrypt.generate_password_hash('vacop_admin_2026').decode('utf-8')
+                    admin = User(username='admin', password_hash=hashed_pw, role='admin')
+                    db.session.add(admin)
+                    db.session.commit()
+                    print("Admin créé.")
+                return
+            except OperationalError:
+                print("DB pas encore prête, attente 2s...")
+                time.sleep(2)
 
 if __name__ == "__main__":
     seed_database()
